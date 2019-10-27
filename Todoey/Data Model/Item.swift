45a204d51c6061b45e45e08251f95249df696252
Artur Franca Cerveira <artfranca@t-online.de>
2019-10-27 11:24:05 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Artur Franca Cerveira on 27.10.19.
+//  Copyright © 2019 Artur Franca Cerveira. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
